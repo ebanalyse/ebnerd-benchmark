e9d74695d7ea86253ac1a1c7211a8483a8602a75
johanneskruse <johannes-kruse@hotmail.com>
2024-02-12 17:20:22 +0100

    minor

--- test/bombing/bomb_dataloader.py
+++ test/bombing/bomb_dataloader.py
@@ -2,14 +2,14 @@ from pathlib import Path
 import polars as pl
 import numpy as np
 
-from ebrec.downloads.models.newsrec.dataloader import (
-    NRMSDataLoader,
+from ebrec.models.newsrec.dataloader import (
     LSTURDataLoader,
-    NAMLDataLoader,
+    NRMSDataLoader,
 )
+from ebrec.utils.utils_behaviors import create_user_id_mapping
+from ebrec.utils.utils_articles import create_title_mapping
+from ebrec.utils.utils_python import create_lookup_dict
 
-# from ebrec.newsrec.dataloader import NRMSDataLoader, LSTURDataLoader, NAMLDataLoader
-# from models.newsrec.utils import create_title_mapping, create_user_id_mapping
 # from newssources.utils_polars import create_lookup_dict
 # from pytest.utils import timer_decorator
 from tqdm import tqdm

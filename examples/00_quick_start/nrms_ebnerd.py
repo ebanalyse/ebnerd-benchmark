c749da7c9a478459fae46fe0f6813378fc7c924b
johanneskruse <ubuntu@ip-172-31-3-141.eu-west-1.compute.internal>
2024-05-23 20:44:20 +0000

    adding script

---
+++ examples/00_quick_start/nrms_ebnerd.py
@@ -0,0 +1,163 @@
+from transformers import AutoTokenizer, AutoModel
+from pathlib import Path
+import tensorflow as tf
+import polars as pl
+import os
+
+from ebrec.utils._constants import (
+    DEFAULT_HISTORY_ARTICLE_ID_COL,
+    DEFAULT_CLICKED_ARTICLES_COL,
+    DEFAULT_INVIEW_ARTICLES_COL,
+    DEFAULT_IMPRESSION_ID_COL,
+    DEFAULT_SUBTITLE_COL,
+    DEFAULT_LABELS_COL,
+    DEFAULT_TITLE_COL,
+    DEFAULT_USER_COL,
+)
+
+from ebrec.utils._behaviors import (
+    create_binary_labels_column,
+    sampling_strategy_wu2019,
+    add_known_user_column,
+    add_prediction_scores,
+    truncate_history,
+)
+from ebrec.evaluation import MetricEvaluator, AucScore, NdcgScore, MrrScore
+from ebrec.utils._articles import convert_text2encoding_with_transformers
+from ebrec.utils._polars import concat_str_columns, slice_join_dataframes
+from ebrec.utils._articles import create_article_id_to_value_mapping
+from ebrec.utils._nlp import get_transformers_word_embeddings
+from ebrec.utils._python import write_submission_file, rank_predictions_by_score
+
+from ebrec.models.newsrec.dataloader import NRMSDataLoader
+from ebrec.models.newsrec.model_config import hparams_nrms
+from ebrec.models.newsrec import NRMSModel
+
+
+def ebnerd_from_path(path: Path, history_size: int = 30) -> pl.DataFrame:
+    """
+    Load ebnerd - function
+    """
+    df_history = (
+        pl.scan_parquet(path.joinpath("history.parquet"))
+        .select(DEFAULT_USER_COL, DEFAULT_HISTORY_ARTICLE_ID_COL)
+        .pipe(
+            truncate_history,
+            column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+            history_size=history_size,
+            padding_value=0,
+            enable_warning=False,
+        )
+    )
+    df_behaviors = (
+        pl.scan_parquet(path.joinpath("behaviors.parquet"))
+        .collect()
+        .pipe(
+            slice_join_dataframes,
+            df2=df_history.collect(),
+            on=DEFAULT_USER_COL,
+            how="left",
+        )
+    )
+    return df_behaviors
+
+
+PATH = Path("~/ebnerd_data")
+DATASPLIT = "ebnerd_small"
+
+COLUMNS = [
+    DEFAULT_USER_COL,
+    DEFAULT_HISTORY_ARTICLE_ID_COL,
+    DEFAULT_INVIEW_ARTICLES_COL,
+    DEFAULT_CLICKED_ARTICLES_COL,
+    DEFAULT_IMPRESSION_ID_COL,
+]
+HISTORY_SIZE = 10
+N_SAMPLES = 100
+
+df_train = (
+    ebnerd_from_path(PATH.joinpath(DATASPLIT, "train"), history_size=HISTORY_SIZE)
+    .select(COLUMNS)
+    .pipe(
+        sampling_strategy_wu2019,
+        npratio=4,
+        shuffle=True,
+        with_replacement=True,
+        seed=123,
+    )
+    .pipe(create_binary_labels_column)
+    .sample(n=N_SAMPLES)
+)
+df_validation = (
+    ebnerd_from_path(PATH.joinpath(DATASPLIT, "validation"), history_size=HISTORY_SIZE)
+    .select(COLUMNS)
+    .pipe(create_binary_labels_column)
+    .sample(n=N_SAMPLES)
+)
+df_articles = pl.read_parquet(PATH.joinpath("articles.parquet"))
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"  # or "true"
+TRANSFORMER_MODEL_NAME = "FacebookAI/xlm-roberta-base"
+TEXT_COLUMNS_TO_USE = [DEFAULT_TITLE_COL, DEFAULT_SUBTITLE_COL]
+MAX_TITLE_LENGTH = 30
+
+# LOAD HUGGINGFACE:
+transformer_model = AutoModel.from_pretrained(TRANSFORMER_MODEL_NAME)
+transformer_tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_MODEL_NAME)
+
+# We'll init the word embeddings using the
+word2vec_embedding = get_transformers_word_embeddings(transformer_model)
+#
+df_articles, cat_cal = concat_str_columns(df_articles, columns=TEXT_COLUMNS_TO_USE)
+df_articles, token_col_title = convert_text2encoding_with_transformers(
+    df_articles, transformer_tokenizer, cat_cal, max_length=MAX_TITLE_LENGTH
+)
+del transformer_model, transformer_tokenizer
+# =>
+article_mapping = create_article_id_to_value_mapping(
+    df=df_articles, value_col=token_col_title
+)
+
+train_dataloader = NRMSDataLoader(
+    behaviors=df_train,
+    article_dict=article_mapping,
+    unknown_representation="zeros",
+    history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+    eval_mode=False,
+    batch_size=2,
+)
+val_dataloader = NRMSDataLoader(
+    behaviors=df_validation,
+    article_dict=article_mapping,
+    unknown_representation="zeros",
+    history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+    eval_mode=True,
+    batch_size=32,
+)
+
+
+MODEL_NAME = "NRMS"
+LOG_DIR = f"downloads/runs/{MODEL_NAME}"
+MODEL_WEIGHTS = f"downloads/data/state_dict/{MODEL_NAME}/weights"
+
+# CALLBACKS
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
+modelcheckpoint = tf.keras.callbacks.ModelCheckpoint(
+    filepath=MODEL_WEIGHTS, save_best_only=True, save_weights_only=True, verbose=1
+)
+
+hparams_nrms.history_size = HISTORY_SIZE
+model = NRMSModel(
+    hparams=hparams_nrms,
+    word2vec_embedding=word2vec_embedding,
+    seed=42,
+)
+hist = model.model.fit(
+    train_dataloader,
+    validation_data=val_dataloader,
+    epochs=10,
+    callbacks=[tensorboard_callback, early_stopping, modelcheckpoint],
+)
+_ = model.model.load_weights(filepath=MODEL_WEIGHTS)

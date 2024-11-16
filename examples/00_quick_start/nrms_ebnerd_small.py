0fafefb3492513d765eb9b71ca2d97e1b326223d
johanneskruse <johannes-kruse@hotmail.com>
2024-11-16 08:06:33 +0000

    updates to flow

--- examples/00_quick_start/nrms_ebnerd_small.py
+++ examples/00_quick_start/nrms_ebnerd_small.py
@@ -6,16 +6,7 @@ import polars as pl
 import gc
 import os
 
-from ebrec.utils._constants import (
-    DEFAULT_HISTORY_ARTICLE_ID_COL,
-    DEFAULT_CLICKED_ARTICLES_COL,
-    DEFAULT_INVIEW_ARTICLES_COL,
-    DEFAULT_IMPRESSION_ID_COL,
-    DEFAULT_SUBTITLE_COL,
-    DEFAULT_LABELS_COL,
-    DEFAULT_TITLE_COL,
-    DEFAULT_USER_COL,
-)
+from ebrec.utils._constants import *
 
 from ebrec.utils._behaviors import (
     create_binary_labels_column,
@@ -34,10 +25,6 @@ from ebrec.utils._python import write_submission_file, rank_predictions_by_score
 from ebrec.models.newsrec.dataloader import NRMSDataLoader, NRMSDataLoaderPretransform
 from ebrec.models.newsrec.model_config import hparams_nrms
 from ebrec.models.newsrec import NRMSModel
-
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-# conda activate ./venv/; python examples/00_quick_start/nrms_ebnerd_small.py
 
 
 def ebnerd_from_path(path: Path, history_size: int = 30) -> pl.DataFrame:
@@ -68,6 +55,9 @@ def ebnerd_from_path(path: Path, history_size: int = 30) -> pl.DataFrame:
     return df_behaviors
 
 
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+# conda activate ./venv/; python examples/00_quick_start/nrms_ebnerd_small.py
+
 PATH = Path("~/ebnerd_data").expanduser()
 DUMP_DIR = Path("ebnerd_predictions")
 DUMP_DIR.mkdir(exist_ok=True, parents=True)
@@ -83,10 +73,9 @@ EPOCHS = 5
 
 TEST_SAMPLES = 100_000
 
-# 0.5549772142638285
-
 MAX_TITLE_LENGTH = 30
 HISTORY_SIZE = 20
+NPRATIO = 4
 
 hparams_nrms.title_size = MAX_TITLE_LENGTH
 hparams_nrms.history_size = HISTORY_SIZE
@@ -97,14 +86,14 @@ hparams_nrms.attention_hidden_dim = 200
 
 hparams_nrms.optimizer = "adam"
 hparams_nrms.loss = "cross_entropy_loss"
-hparams_nrms.dropout = 0.2
+hparams_nrms.dropout = 0.35
 hparams_nrms.learning_rate = 1e-5
+
+WITH_REPLACEMENT = True
+MIN_VALS = 0
 
 BS_TRAIN = 32
 BS_TEST = 32
-
-NPRATIO = 4
-# 0.0001 + 0.2 = 0.55 AUC
 
 COLUMNS = [
     DEFAULT_USER_COL,
@@ -123,13 +112,13 @@ df_train = (
             pl.col(DEFAULT_INVIEW_ARTICLES_COL).list.len()
             - pl.col(DEFAULT_CLICKED_ARTICLES_COL).list.len()
         )
-        > NPRATIO
+        > MIN_VALS
     )
     .pipe(
         sampling_strategy_wu2019,
         npratio=NPRATIO,
-        shuffle=False,
-        with_replacement=False,
+        shuffle=True,
+        with_replacement=WITH_REPLACEMENT,
         seed=123,
     )
     .pipe(create_binary_labels_column)
@@ -138,7 +127,7 @@ df_train = (
 # =>
 df_val = ebnerd_from_path(
     PATH.joinpath(DATASPLIT, "validation"), history_size=HISTORY_SIZE
-)[:TEST_SAMPLES].select(COLUMNS)
+).select(COLUMNS)[:TEST_SAMPLES]
 
 df_val, df_test = split_df(df_val, fraction=0.5, seed=123)
 
@@ -148,40 +137,44 @@ df_val = (
             pl.col(DEFAULT_INVIEW_ARTICLES_COL).list.len()
             - pl.col(DEFAULT_CLICKED_ARTICLES_COL).list.len()
         )
-        > NPRATIO
+        > MIN_VALS
     )
     .pipe(
         sampling_strategy_wu2019,
         npratio=NPRATIO,
         shuffle=True,
-        with_replacement=False,
+        with_replacement=WITH_REPLACEMENT,
         seed=123,
     )
     .pipe(create_binary_labels_column)
 )
 
-df_test = df_test.pipe(create_binary_labels_column)
-
-# df_test = df_validation
-# df_train = df_train[:100]
-# df_validation = df_validation[:100]
-# df_test = df_test[:100]
-df_articles = pl.read_parquet(PATH.joinpath("articles.parquet"))
+df_test = df_test.pipe(create_binary_labels_column, shuffle=False)
 
 # =>
+df_articles = pl.read_parquet(PATH.joinpath("articles.parquet"))
 TRANSFORMER_MODEL_NAME = "Maltehb/danish-bert-botxo"
-TEXT_COLUMNS_TO_USE = [DEFAULT_SUBTITLE_COL, DEFAULT_TITLE_COL]
 
+TEXT_COLUMNS_TO_USE = [DEFAULT_TITLE_COL, DEFAULT_SUBTITLE_COL, DEFAULT_BODY_COL]
+article_path = PATH.joinpath(
+    f"articles_{TRANSFORMER_MODEL_NAME.replace('/', '_')}_{MAX_TITLE_LENGTH}.parquet"
+)
 # LOAD HUGGINGFACE:
 transformer_model = AutoModel.from_pretrained(TRANSFORMER_MODEL_NAME)
 transformer_tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_MODEL_NAME)
 
 word2vec_embedding = get_transformers_word_embeddings(transformer_model)
 #
-df_articles, cat_cal = concat_str_columns(df_articles, columns=TEXT_COLUMNS_TO_USE)
-df_articles, token_col_title = convert_text2encoding_with_transformers(
-    df_articles, transformer_tokenizer, cat_cal, max_length=MAX_TITLE_LENGTH
-)
+if not article_path.exists():
+    df_articles, cat_cal = concat_str_columns(df_articles, columns=TEXT_COLUMNS_TO_USE)
+    df_articles, token_col_title = convert_text2encoding_with_transformers(
+        df_articles, transformer_tokenizer, cat_cal, max_length=MAX_TITLE_LENGTH
+    )
+    df_articles.write_parquet(article_path)
+else:
+    df_articles = pl.read_parquet(article_path)
+    token_col_title = df_articles.columns[-1]
+
 # =>
 article_mapping = create_article_id_to_value_mapping(
     df=df_articles, value_col=token_col_title
@@ -197,16 +190,16 @@ train_dataloader = NRMSDataLoaderPretransform(
     batch_size=BS_TRAIN,
 )
 
-val_dataloader = NRMSDataLoader(
+val_dataloader = NRMSDataLoaderPretransform(
     behaviors=df_val,
     article_dict=article_mapping,
     unknown_representation="zeros",
     history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
     eval_mode=False,
-    batch_size=BS_TRAIN,
+    batch_size=BS_TEST,
 )
 
-test_dataloader = NRMSDataLoader(
+test_dataloader = NRMSDataLoaderPretransform(
     behaviors=df_test,
     article_dict=article_mapping,
     unknown_representation="zeros",
@@ -215,39 +208,72 @@ test_dataloader = NRMSDataLoader(
     batch_size=BS_TEST,
 )
 
+train_dataloader_test = NRMSDataLoaderPretransform(
+    behaviors=df_train,
+    article_dict=article_mapping,
+    unknown_representation="zeros",
+    history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+    eval_mode=True,
+    batch_size=BS_TEST,
+)
+
 # CALLBACKS
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor="val_auc", mode="max", patience=20, restore_best_weights=True
+)
 modelcheckpoint = tf.keras.callbacks.ModelCheckpoint(
-    filepath=MODEL_WEIGHTS, save_best_only=True, save_weights_only=True, verbose=1
+    filepath=MODEL_WEIGHTS,
+    monitor="val_auc",
+    mode="max",
+    save_best_only=True,
+    save_weights_only=True,
+    verbose=1,
 )
 lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(
-    monitor="val_loss", factor=0.5, patience=2, min_lr=1e-6
+    monitor="val_loss", mode="min", factor=0.5, patience=2, min_lr=1e-6
 )
-callbacks = [lr_scheduler, early_stopping, modelcheckpoint]
-
-word2vec_embedding.shape
+callbacks = [lr_scheduler, early_stopping, modelcheckpoint, tensorboard_callback]
 
 model = NRMSModel(
     hparams=hparams_nrms,
-    # word2vec_embedding=word2vec_embedding,
-    word_emb_dim=768,
-    vocab_size=250002,
+    word2vec_embedding=word2vec_embedding,
     seed=42,
 )
+model.model.compile(
+    optimizer=model.model.optimizer,
+    loss="categorical_crossentropy",
+    metrics=["AUC"],
+)
+
 hist = model.model.fit(
     train_dataloader,
     validation_data=val_dataloader,
-    epochs=EPOCHS,
+    epochs=15,
     callbacks=callbacks,
 )
+print("loading model...")
+model.model.load_weights(MODEL_WEIGHTS)
 
-# print("saving model...")
-# model.model.save_weights(MODEL_WEIGHTS)
-# #
-# print("loading model...")
-# model.model.load_weights(MODEL_WEIGHTS)
 
+pred_test = model.scorer.predict(train_dataloader_test)
+print("Adding prediction-scores...")
+df_train_test = add_prediction_scores(df_train, pred_test.tolist())
+
+print("Evaluating...")
+metrics = MetricEvaluator(
+    labels=df_train_test["labels"].to_list(),
+    predictions=df_train_test["scores"].to_list(),
+    metric_functions=[
+        AucScore(),
+        # MrrScore(),
+        # NdcgScore(k=5),
+        # NdcgScore(k=10),
+    ],
+)
+metrics.evaluate()
+print(metrics.evaluations)
+# 0.7383
 # =>
 pred_test = model.scorer.predict(test_dataloader)
 print("Adding prediction-scores...")

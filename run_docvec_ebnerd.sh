9925236ec7f0f6f36bcd1afcfc606a13a4150edd
johanneskruse <johannes-kruse@hotmail.com>
2025-02-08 17:58:35 +0000

    minor

--- run_docvec_ebnerd.sh
+++ run_docvec_ebnerd.sh
@@ -4,39 +4,36 @@
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate ./venv
 
-python examples/reproducibility_scripts/ebnerd_lstur_npa_docvec.py \
-    --model NPADocVec \
-    --title_size 300 \
-    --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
+# NRMS
+python examples/reproducibility_scripts/ebnerd_nrms_docvec_hist.py \
     --datasplit ebnerd_small \
+    --title_size 768 \
+    --document_embeddings FacebookAI_xlm_roberta_base/xlm_roberta_base.parquet \
     --epochs 5 \
     --bs_train 32 \
     --history_size 20 \
     --npratio 4 \
+    --head_num 16 \
+    --head_dim 16 \
     --attention_hidden_dim 200 \
-    --user_emb_dim 400 \
-    --filter_num 400 \
     --newsencoder_units_per_layer 256 256 256 \
     --learning_rate 1e-4 \
     --dropout 0.2 \
     --newsencoder_l2_regularization 1e-4
 
-
-# NRMS
-# python examples/reproducibility_scripts/ebnerd_nrms_docvec_hist.py \
-#     --datasplit ebnerd_small \
+# python examples/reproducibility_scripts/ebnerd_lstur_npa_docvec.py \
+#     --model NPADocVec \
 #     --title_size 300 \
 #     --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
+#     --datasplit ebnerd_small \
 #     --epochs 5 \
 #     --bs_train 32 \
 #     --history_size 20 \
 #     --npratio 4 \
-#     --head_num 16 \
-#     --head_dim 16 \
 #     --attention_hidden_dim 200 \
+#     --user_emb_dim 400 \
+#     --filter_num 400 \
 #     --newsencoder_units_per_layer 256 256 256 \
 #     --learning_rate 1e-4 \
 #     --dropout 0.2 \
 #     --newsencoder_l2_regularization 1e-4
-
-

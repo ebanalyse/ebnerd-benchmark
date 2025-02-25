1fd1241c063f5f8d48638b512792af5617fc41bd
Ubuntu <ubuntu@ip-10-0-46-113.eu-west-1.compute.internal>
2025-02-25 16:11:03 +0000

    sh flow

--- run_docvec_ba.sh
+++ run_docvec_ba.sh
@@ -6,19 +6,19 @@ conda activate ./venv
 
 # NRMS
 python examples/reproducibility_scripts/ebnerd_nrms_ba.py \
-    --title_size 768 \
+    --title_size 300 \
     --history_size 20 \
     --newsencoder_units_per_layer 256 256 256 \
-    --document_embeddings Ekstra_Bladet_contrastive_vector/contrastive_vector.parquet
+    --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
 
 python examples/reproducibility_scripts/ebnerd_nrms_ba.py \
-    --title_size 768 \
+    --title_size 300 \
     --history_size 20 \
     --newsencoder_units_per_layer 512 512 512 \
-    --document_embeddings Ekstra_Bladet_contrastive_vector/contrastive_vector.parquet
+    --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
 
 python examples/reproducibility_scripts/ebnerd_nrms_ba.py \
-    --title_size 768 \
+    --title_size 300 \
     --history_size 10 \
     --newsencoder_units_per_layer 256 256 256 \
-    --document_embeddings Ekstra_Bladet_contrastive_vector/contrastive_vector.parquet
+    --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \

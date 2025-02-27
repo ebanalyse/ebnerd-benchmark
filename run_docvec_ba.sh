e72bf5b79619ba9a7627cee092f47b04245cf472
johanneskruse <johannes-kruse@hotmail.com>
2025-02-27 09:33:33 -0800

    script to run

--- run_docvec_ba.sh
+++ run_docvec_ba.sh
@@ -22,3 +22,27 @@ python examples/reproducibility_scripts/ebnerd_nrms_ba.py \
     --history_size 10 \
     --newsencoder_units_per_layer 256 256 256 \
     --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
+
+python examples/reproducibility_scripts/ebnerd_nrms_ba.py \
+    --title_size 768 \
+    --history_size 20 \
+    --newsencoder_units_per_layer 256 256 256 \
+    --document_embeddings Ekstra_Bladet_contrastive_vector/contrastive_vector.parquet 
+
+# LSTUR
+python examples/reproducibility_scripts/ebnerd_lstur_npa_ba.py \
+    --model LSTURDocVec \
+    --title_size 300 \
+    --history_size 20 \
+    --newsencoder_units_per_layer 256 256 256 \
+    --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
+    --debug
+
+# NPA
+python examples/reproducibility_scripts/ebnerd_lstur_npa_ba.py \
+    --model NPADocVec \
+    --title_size 300 \
+    --history_size 20 \
+    --newsencoder_units_per_layer 256 256 256 \
+    --document_embeddings Ekstra_Bladet_word2vec/document_vector.parquet \
+    --debug
